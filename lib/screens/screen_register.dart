c1088ca8a74b8dc98b30a15daddf6e760d83acc8
RafBel94 <beltran.caraf23@cadiz.salesianos.edu>
2025-02-18 13:20:58 +0100

    Started register functionality

--- lib/screens/screen_register.dart
+++ lib/screens/screen_register.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import '../widgets/shared/bg_auth.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
---
+++ lib/widgets/forms/form_register.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:libridex_mobile/actions/action_register.dart';
+
+class FormRegister extends StatefulWidget {
+  const FormRegister({super.key});
+
+  @override
+  _FormRegisterState createState() => _FormRegisterState();
+}
+
+class _FormRegisterState extends State<FormRegister> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _rePasswordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Center(
+      child: Container(
+        width: size.width,
+        padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: size.width * 0.15),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+
+              const SizedBox(height: 50),
+
+              const Icon(Icons.account_circle, size: 100),
+
+              const SizedBox(height: 20),
+
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+              ),
+
+              const SizedBox(height: 20),
+
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+
+              TextField(
+                controller: _rePasswordController,
+                decoration: const InputDecoration(labelText: 'Repeat password'),
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 40),
+
+              ElevatedButton(
+                onPressed: () => registerAction(context, _emailController.text, _passwordController.text, _rePasswordController.text),
+                child: const Text('Submit', style: TextStyle(
+                  fontSize: 16,
+                ),),
+              ),
+
+              const SizedBox(height: 20,)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
