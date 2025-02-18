aede154f1d1d17c50d27944d4a6b30ba39cd5fc0
RafBel94 <beltran.caraf23@cadiz.salesianos.edu>
2025-02-18 13:10:30 +0100

    Refactorized project structure

--- lib/screens/registerScreen.dart
+++ lib/screens/registerScreen.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import '../widgets/shared/bg_auth.dart';
-import '../widgets/forms/form_login.dart';
 
-class ScreenLogin extends StatelessWidget {
-  const ScreenLogin({super.key});
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Stack(
         children: <Widget>[
+          // Background image
           BgAuth(),
-          Center(child: FormLogin()),
+          Center(child: Text('Hello')),
         ],
       ),
     );
--- lib/widgets/forms/form_login.dart
+++ lib/widgets/forms/form_login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../actions/action_login.dart';
+import '../../actions/action_login.dart';
 
 class FormLogin extends StatefulWidget {
   const FormLogin({super.key});
